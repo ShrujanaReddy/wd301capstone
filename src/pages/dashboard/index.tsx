28560ef9208efdae3115834d6f6a001c902dbb92
shrujana <shrujanareddy7@gmail.com>
2024-04-03 00:21:20 +0530

    showing your news section only to signed users

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Appbar from '../../layouts/account/Appbar';
 import Matches from '../matches';
 import Articles from '../articles';
 import Favorites from '../favorites';
 const Dashboard: React.FC = () => {
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+
+  useEffect(() => {
+    setIsUserSignedIn(!!localStorage.getItem('authToken'));
+  }, []);
   return (
     <>
     <Appbar />
     <Matches />
-    <Favorites />
+    {isUserSignedIn && <Favorites />}    
     <Articles />
     </>
   );
--- src/pages/favorites/FavList.tsx
+++ src/pages/favorites/FavList.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, Suspense } from "react";
 import { fetchArticles } from "../../context/articles/actions";
 import { useArticlesDispatch } from "../../context/articles/context";
+import { usePreferencesDispatch } from "../../context/preferences/context";
+import { fetchPreferences } from "../../context/preferences/actions";
 import FavListItems from "./FavListItems";
 import ErrorBoundary from "../../components/ErrorBoundary";
 
 const ArticleList: React.FC = () => {
+  const authtoken=localStorage.getItem('authToken') || ""
   const dispatch = useArticlesDispatch();
-  
+  const pdispatch=usePreferencesDispatch();
+  useEffect(()=> {
+    fetchPreferences(pdispatch,authtoken);
+  }, [pdispatch]);
   useEffect(() => {
     fetchArticles(dispatch);
   }, [dispatch]);
